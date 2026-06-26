9a358887d53461f2ba5920648a60676e154f204d
Matheus C Pereira <mcostap80@gmail.com>
2026-06-25 23:07:12 -0300

    feat: Docker image

--- apps/m7-cms-api/src/main.ts
+++ apps/m7-cms-api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3002'],
+    origin: true,
     credentials: true,
   });
 
